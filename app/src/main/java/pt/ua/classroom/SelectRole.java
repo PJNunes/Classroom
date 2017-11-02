926e732a35cebb7d212ab3713a2b64d3806635ec
akuatiko95 <lasconchasgonzalo@gmail.com>
2017-11-02 17:16:01 +0000

    SelectRole design

--- app/src/main/java/pt/ua/classroom/SelectRole.java
+++ app/src/main/java/pt/ua/classroom/SelectRole.java
@@ -1,13 +1,59 @@
 package pt.ua.classroom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class SelectRole extends AppCompatActivity {
+public class SelectRole extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_role);
+
+        //Buttons
+        Button buttonTeacher = (Button) findViewById(R.id.button_teacher);
+        buttonTeacher.setOnClickListener(this);
+
+
+        Button buttonStudent = (Button) findViewById(R.id.button_student);
+        buttonStudent.setOnClickListener(this);
+
     }
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.button_teacher:
+                teacherActivity();
+                Log.d("Role --> teacher", "se está ejecutando");
+                finish();
+                break;
+
+            case R.id.button_student:
+                studentActivity();
+                Log.d("Role --> student", "se está ejecutando");
+                finish();
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+
+    public void teacherActivity(){
+        startActivity(new Intent(this, TeacherActivity.class));
+    }
+
+    public void studentActivity(){
+        startActivity(new Intent(this, StudentActivity.class));
+    }
+
 }
