ffa0f833031756d522665b28ce005f78b97d400e
Pedro Nunes <pjnunes@ua.pt>
2017-11-08 10:41:45 +0000

    List of classes

--- app/src/main/java/pt/ua/classroom/TeacherActivityMenu.java
+++ app/src/main/java/pt/ua/classroom/TeacherActivityMenu.java
@@ -1,8 +1,13 @@
 package pt.ua.classroom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TeacherActivityMenu extends AppCompatActivity {
 
@@ -13,5 +18,35 @@ public class TeacherActivityMenu extends AppCompatActivity {
 
         TextView tv= (TextView) findViewById(R.id.classeName);
         tv.setText(Database.getClasseName());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.log_out:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(this, Login.class));
+                finish();
+            case R.id.swap_role:
+                studentActivity();
+                finish();
+            case R.id.default_role:
+                Database.setRole("teacher");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void studentActivity(){
+        startActivity(new Intent(this, StudentActivity.class));
     }
 }
